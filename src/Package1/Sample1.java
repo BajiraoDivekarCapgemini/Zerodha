2af9079be302c6c6ea7f0204429f25efd7f71de2
baji <baji@baji-PC>
2022-08-29 08:29:37 +0530

    My First Project On GitHub

---
+++ src/Package1/Sample1.java
@@ -0,0 +1,14 @@
+package Package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\baji\\Downloads\\geckodriver-v0.31.0-win32\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+
+}
